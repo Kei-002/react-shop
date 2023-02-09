ac216727c87d7d19adb14b7e29795795e119f6a7
Khen Vargas <khenchristian.vargas@tup.edu.ph>
2023-02-09 10:37:02 +0800

    added lecture copy

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,15 @@
-const app = require('./app');
+const app = require("./app");
 
-const connectDatabase = require('./config/database')
+const connectDatabase = require("./config/database");
 
+const dotenv = require("dotenv");
 
-
-const dotenv = require('dotenv');
-
-
-
-dotenv.config({path: './config/.env'})
-
-
+dotenv.config({ path: "./config/.env" });
 
 connectDatabase();
 
-
-
 app.listen(process.env.PORT, () => {
-
-	console.log(`server started on port:' ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
-
+  console.log(
+    `server started on port:' ${process.env.PORT} in ${process.env.NODE_ENV} mode`
+  );
 });

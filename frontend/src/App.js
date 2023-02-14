a682768412a7291ec697cbf28eb3a568c465759a
Khen Vargas <khenchristian.vargas@tup.edu.ph>
2023-02-14 09:36:25 +0800

    added frontend stuff

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import Home from './components/Home'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+//get images from github repo
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+root.render(
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+);
+// ReactDOM.render(
+//   // <React.StrictMode>
+//     <App />
+//   {/* </React.StrictMode>, */}
+//   document.getElementById('root')
+// );
+
+// ReactDOM.render

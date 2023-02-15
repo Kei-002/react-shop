c6d8743f64811118431a69440b3cec57b662872c
Khen Vargas <khenchristian.vargas@tup.edu.ph>
2023-02-15 23:05:40 +0800

    none changed(2)

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -2,9 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-const reducer = combineReducers({
-
-})
+const reducer = combineReducers({});
 
 let initialState = {
 
